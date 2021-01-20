7767cf135106806344a06a6a15f7215db6337864
Emmanuel <52857931+eeTiboah@users.noreply.github.com>
2021-01-20 11:18:37 +0100

    Chapter 4 Roll The Die Game edit

--- src/chapter4/RollTheDieGame.java
+++ src/chapter4/RollTheDieGame.java
@@ -3,18 +3,18 @@ package chapter4;
 import java.util.Random;
 
 public class RollTheDieGame {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Random random = new Random();
         int totalRoll = 5;
         int boardSpace = 20;
         int space=0;
-
+        
         for (int i=0; i<totalRoll; i++) {
             int die = random.nextInt(6) + 1;
             space = space + die;
             int spaceRemaining = boardSpace - die;
             boardSpace = spaceRemaining;
-            if (i == 4 && (space != 20)) {
+            if (i == (totalRoll-1) && (space != 20)) {
                 System.out.println("Roll #" + (i + 1) + " You have rolled a " + die + ". You are now on space " + space +
                         " You lose!!!");
             } else if (space == 20) {
